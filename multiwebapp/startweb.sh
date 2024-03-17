94e6cfb86a7b86cba8193dd4c2f2dc125bf249ca
ayushsoni9784 <ayushsoni626@gmail.com>
2024-03-18 02:30:13 +0530

    Multiwebapp Dockerfile

---
+++ multiwebapp/startweb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if	[ "$x" == "webapp1" ]
+then
+	cp -rf /mycode/webapp1/*   /var/www/html/
+	httpd -DFOREGROUND
+
+elif	[ "$x" == "webapp2" ]
+then
+        cp -rf /mycode/webapp2/*   /var/www/html/
+        httpd -DFOREGROUND
+else 
+	echo "Sorry no webapp found" > /var/www/html/index.html
+	httpd -DFOREGROUND
+
+fi
